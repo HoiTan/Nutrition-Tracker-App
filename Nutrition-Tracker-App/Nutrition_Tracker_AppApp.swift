6cc2a8d4021d626739ae384db9314000b4ffe04d
Hoimau Tan <tanhoimau@gmail.com>
2025-09-22 16:17:18 -0700

    Initial Commit

---
+++ Nutrition-Tracker-App/Nutrition_Tracker_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nutrition_Tracker_AppApp.swift
+//  Nutrition-Tracker-App
+//
+//  Created by Hoi Mau Tan on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Nutrition_Tracker_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
